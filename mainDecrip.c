6c00a4b5471a2ff1862941f01d6efea3a1161fdc
frankemax <42683590+frankemax@users.noreply.github.com>
2019-05-17 05:38:22 -0300

    Update mainDecrip.c

--- mainDecrip.c
+++ mainDecrip.c
@@ -67,6 +67,7 @@ void decodificaMensagem(char message[], int cifra)
     }
 }
 
+
 // Um pixel RGB
 typedef struct
 {
@@ -110,12 +111,18 @@ int main(int argc, char** argv)
 
 
     // Interacao
+    char message[100];
     char password[100];
+    char hex[100];
 
-    printf("Digite a senha:\n ");
+    printf("Digite a mensagem: \n");
+    scanf("%s", message);
+
+    printf("Digite uma senha:\n ");
     scanf("%s", password);
 
-    printf("Senha digitada: %s\n", password);
+    printf("Minha Mensagem: %s\n", message);
+    printf("Minha Senha: %s\n", password);
 
     //%%%%%%%%%%%%%%%%%%%%%%%%%% Designar o intervalo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     int media=0;
@@ -131,23 +138,114 @@ int main(int argc, char** argv)
     int intervalo=media/strlen(password);
     printf("Intervalo = %d\n", intervalo);
     //%%%%%%%%%%%%%%%%%%%%%%%%%%%% Designar o inicio %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
     int start= password[0];
     printf("Start = %d\n", start);
     //%%%%%%%%%%%%%%%%%%%%%%%%%%%% Designar quantas casas a cifra irá avançar %%%%%%%%%%%%%%%%
     int cifra= 10;
     printf("Cifra = %d\n", cifra);
 
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%% Cifrar a mensagem- codifica-la%%%%%%%%%%%%%%%%
+    codificaMensagem(message,cifra);
+    printf("Mensagem COD: %s\n", message);
 
+    //decodificaMensagem(message,cifra);
+    //printf("Mensagem DECOD: %s\n", message);
 
-
-    //%%%%%%%%%%%%%%%%%%%%%%%%%%%% Pegar na imagem a mensagem cifrada %%%%%%%%%%%%%%%%
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%% alterar na imagem com os bits %%%%%%%%%%%%%%%%
 
     printf("\nIMG antes\n");
-    for(int i=start; i<pic.height*pic.width ; i=i+intervalo){
-    if((((pic.img[i].r) << 4)==0) && ((pic.img[i].g) << 4) == 0) {
-        break;
+    for(int i=start; i<pic.height*pic.width ; i=i+intervalo)
+    {
+        if(i<=start+intervalo*strlen(message))
+        {
+            printf("[%d %d %0d] ", pic.img[i].r, pic.img[i].g, pic.img[i].b);
+        }
     }
-        if(i<start+intervalo*100)
+
+    unsigned int pix,pix2, pospix, pospix2, put, auxiliar;
+    int z=0;
+
+    printf("\n zerando os quatro ultimos e deixando certo\n");
+    for(int i=start; i<pic.height*pic.width ; i=i+intervalo)
+    {
+        if(i<=start+intervalo*strlen(message))
+        {
+            pix=pic.img[i].r;
+            pix2=pic.img[i].g;
+            //zerei os ultimos 4 bits do red e blue
+            pospix = pix & 0XF0;
+            pospix2 = pix2 & 0XF0;
+            //printf("pos pix = %d\n",pospix);
+            pic.img[i].r=pospix;
+            pic.img[i].g=pospix2;
+
+            // colocar os ultimos 4 bits do red e blue
+            auxiliar = message[z];
+            pospix = (pic.img[i].r) | (message[z] >>4);
+            pospix2 = (pic.img[i].g) | (auxiliar & 0X0F);
+
+            pic.img[i].r=pospix;
+            pic.img[i].g=pospix2;
+
+            printf("[%d %d %0d] ", pic.img[i].r, pic.img[i].g, pic.img[i].b);
+            //printf("entrou\n");
+            z++;
+
+        }
+
+    }
+    SOIL_save_image("saidas.jpg", SOIL_SAVE_TYPE_BMP, pic.width, pic.height, 3, pic.img);
+    free(pic.img);
+
+
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% AGORA DECRIPTADOR %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    load("saida.bmp", &pic);
+
+    // Interacao
+    char password2[100];
+
+    printf("Digite a senha:\n ");
+    scanf("%s", password2);
+
+    printf("Senha digitada: %s\n", password2);
+
+    //%%%%%%%%%%%%%%%%%%%%%%%%%% Designar o intervalo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    int media2=0;
+    int ascii2=0;
+    for (int i =0; i< strlen(password2); i++){
+        ascii2=password2[i];
+        //printf("valor do ascii: %d\n",ascii);
+        media2=media2+ascii2;
+        //printf("ASCII value of %c = %d\n", password[i], password[i]);
+    }
+    int intervalo2=media2/strlen(password2);
+    printf("Intervalo = %d\n", intervalo2);
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%% Designar o inicio %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    int start2= password[0];
+    printf("Start = %d\n", start2);
+
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%% Designar quantas casas a cifra irá avançar %%%%%%%%%%%%%%%%
+    int cifra2= 10;
+    printf("Cifra = %d\n", cifra2);
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%% Pegar na imagem a mensagem cifrada %%%%%%%%%%%%%%%%
+
+    unsigned int pix1,pix21, pospix1, pospix21, put1, auxiliar1;
+    printf("\nIMG antes\n");
+    for(int i=start2; i<pic.height*pic.width ; i=i+intervalo2)
+    {
+        pix1 = pic.img[i].r;
+        pix21 = pic.img[i].g;
+        pix1 = pix1 << 4;
+        pix21 = pix21 << 4;
+        if((pix1=='\0') && (pix21 == '\0'))
+        {
+            printf("entra aqui?? \n");
+            break;
+        }
+        if(i<start2+intervalo2*100)
         {
             printf("[%d %d %0d] ", pic.img[i].r, pic.img[i].g, pic.img[i].b);
         }
@@ -156,8 +254,13 @@ int main(int argc, char** argv)
 
 
 
-    printf("\n");
 
 
-    free(pic.img);
+
+
+
+
+
+
+
 }
